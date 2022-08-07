8215b6b465e38f43e8b1abd4275da3de4b29195f
Balaji Ravindaran <66238647+BalajiRavindaran@users.noreply.github.com>
2022-08-07 13:08:02 +0530

    Add files via upload

---
+++ Web API/FABRADE/Models/fabradeDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FABRADE.Models
+{
+    public class fabradeDBContext:DbContext
+    {
+        public fabradeDBContext(DbContextOptions<fabradeDBContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<fabradeTransaction>? fabradeTransactions { get; set; }
+    }
+}
